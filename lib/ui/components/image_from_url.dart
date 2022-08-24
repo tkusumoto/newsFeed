a6cdb8c90ff9cafcb1c13d77539e90f07fdf8d4c
Takaaki Kusumoto <kusumoto28@gmail.com>
2022-08-24 14:22:44 +0900

    feat: add cached_network_image

---
+++ lib/ui/components/image_from_url.dart
@@ -0,0 +1,25 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:news_feed/const/strings.dart';
+
+class ImageFromUrl extends ConsumerWidget {
+  final String? imageUrl;
+  const ImageFromUrl({this.imageUrl});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isInvalidUrl =
+        (imageUrl != null) ? imageUrl!.startsWith("http") : false;
+    if (imageUrl == null || imageUrl == Strings.EmptyString || !isInvalidUrl) {
+      return const Icon(Icons.broken_image);
+    } else {
+      return CachedNetworkImage(
+        imageUrl: imageUrl!,
+        placeholder: (context, url) => const CircularProgressIndicator(),
+        errorWidget: (context, url, error) => const Icon(Icons.broken_image),
+        fit: BoxFit.cover,
+      );
+    }
+  }
+}
