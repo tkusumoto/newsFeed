d562dfdbe8e573f2e7f9b20d5221c6c282a92d74
Takaaki Kusumoto <kusumoto28@gmail.com>
2022-08-25 13:54:51 +0900

    feat: about_us page

--- lib/const/strings.dart
+++ lib/const/strings.dart
@@ -7,4 +7,6 @@ class Strings {
   static const String Refresh = "更新";
   static const String DataEmpty = "データはありません";
   static const String EmptyString = "";
+  static const String AnimationStart = "アニメーション開始";
+  static const String WelcomeApp = "Welcome NewsFeed";
 }
--- lib/ui/screens/pages/about_us_page.dart
+++ lib/ui/screens/pages/about_us_page.dart
@@ -1,17 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:news_feed/const/strings.dart';
 
 class AboutUsPage extends HookConsumerWidget {
   const AboutUsPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final textTheme = Theme.of(context).textTheme;
+
+    final selected = useState(false);
+
     return SafeArea(
       child: Scaffold(
-        body: Container(
-          child: Center(
-            child: Text("AboutUs"),
+        floatingActionButton: FloatingActionButton(
+          tooltip: Strings.AnimationStart,
+          onPressed: () => selected.value = !selected.value,
+          child: const Icon(Icons.play_arrow),
+        ),
+        body: Center(
+          child: AnimatedContainer(
+            alignment: selected.value
+                ? Alignment.bottomCenter
+                : AlignmentDirectional.center,
+            duration: const Duration(seconds: 2),
+            curve: Curves.bounceOut,
+            child: Text(
+              Strings.WelcomeApp,
+              style: textTheme.titleLarge?.copyWith(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
           ),
+          // child: Text(Strings.AppName),
         ),
       ),
     );
